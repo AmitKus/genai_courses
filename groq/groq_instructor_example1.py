c1ddf0f3ce5a039fab781d31fa1f98a9832a5280
AmitKus <amit.kushwah@gmail.com>
2024-09-30 14:58:36 +0100

    updat example

--- groq/groq_instructor_example1.py
+++ groq/groq_instructor_example1.py
@@ -9,7 +9,7 @@ client = Groq(
 )
 
 # By default, the patch function will patch the ChatCompletion.create and ChatCompletion.create methods to support the response_model parameter
-client = instructor.from_groq(client, mode=instructor.Mode.TOOLS)
+client = instructor.from_groq(client, mode=instructor.Mode.JSON)
 
 
 # Now, we can use the response_model parameter using only a base model
@@ -17,18 +17,19 @@ client = instructor.from_groq(client, mode=instructor.Mode.TOOLS)
 class UserExtract(BaseModel):
     name: str
     age: int
+    email: str
 
 
 user: UserExtract = client.chat.completions.create(
     model="llama-3.1-70b-versatile",
     response_model=UserExtract,
     messages=[
-        {"role": "user", "content": "Extract jason is 25 years old"},
+        {"role": "user", "content": "Extract jason is 25 years old. His preferred way of contact is jason@gmail.com"},
     ],
 )
 
 assert isinstance(user, UserExtract), "Should be instance of UserExtract"
 assert user.name.lower() == "jason"
 assert user.age == 25
-
+assert user.email == "jason@gmail.com"
 print(user.model_dump_json(indent=2))
--- sambanova/sambanova_instructor_example1.py
+++ sambanova/sambanova_instructor_example1.py
@@ -8,7 +8,6 @@ client = OpenAI(
     api_key=os.getenv("SAMBANOVA_API_KEY"),
     base_url="https://fast-api.snova.ai/v1",
 )
-print(os.getenv("SAMBANOVA_API_KEY"))
 
 # By default, the patch function will patch the ChatCompletion.create and ChatCompletion.create methods to support the response_model parameter
 client = instructor.from_openai(client, mode=instructor.Mode.JSON_SCHEMA)
@@ -19,18 +18,20 @@ client = instructor.from_openai(client, mode=instructor.Mode.JSON_SCHEMA)
 class UserExtract(BaseModel):
     name: str
     age: int
+    email: str
+
 
 
 user: UserExtract = client.chat.completions.create(
     model="llama3-70b",
     response_model=UserExtract,
     messages=[
-        {"role": "user", "content": "Extract jason is 25 years old"},
+        {"role": "user", "content": "Extract jason is 25 years old. His preferred way of contact is jason@gmail.com"},
     ]
 )
 
 assert isinstance(user, UserExtract), "Should be instance of UserExtract"
 assert user.name.lower() == "jason"
 assert user.age == 25
-
+assert user.email == "jason@gmail.com"
 print(user.model_dump_json(indent=2))
